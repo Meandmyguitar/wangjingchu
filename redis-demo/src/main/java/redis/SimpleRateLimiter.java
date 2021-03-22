dabd4e8b90ffbf9c486c4842f6450da6ad9c70b6
wangzhengpeng <wangzhengpeng@huohua.cn>
2021-03-22 13:00:39 +0800

    limiter

---
+++ redis-demo/src/main/java/redis/SimpleRateLimiter.java
@@ -0,0 +1,36 @@
+package redis;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Pipeline;
+import redis.clients.jedis.Response;
+
+public class SimpleRateLimiter {
+
+    private Jedis jedis;
+
+    public SimpleRateLimiter(Jedis jedis) {
+        this.jedis = jedis;
+    }
+
+    public boolean isActionAllowed(String userId, String actionKey, int period, int maxCount) {
+        String key = String.format("hist:%s:%s", userId, actionKey);
+        long nowTs = System.currentTimeMillis();
+        Pipeline pipe = jedis.pipelined();
+        pipe.multi();
+        pipe.zadd(key, nowTs, "" + nowTs);
+        pipe.zremrangeByScore(key, 0, nowTs - period * 1000);
+        Response<Long> count = pipe.zcard(key);
+        pipe.expire(key, period + 1);
+        pipe.exec();
+        pipe.close();
+        return count.get() <= maxCount;
+    }
+
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("127.0.0.1");
+        SimpleRateLimiter limiter = new SimpleRateLimiter(jedis);
+        for (int i = 0; i < 20; i++) {
+            System.out.println(limiter.isActionAllowed("answer", "reply", 60, 5));
+        }
+    }
+}
